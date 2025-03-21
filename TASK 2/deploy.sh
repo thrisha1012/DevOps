9ea0a6a63b143fe2341a83c36f2a858816a2b80a
thrisha1012 <thrishamani2004@gmail.com>
2025-03-21 22:30:51 +0530

    Update deploy.sh

--- TASK 2/deploy.sh
+++ TASK 2/deploy.sh
@@ -2,7 +2,7 @@
     echo hello123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u thrisha1012 -p 
+    docker login -u thrisha1012 -p dckr_pat__FpXRGOJ9p-qgqx0jraSOqitX7E
     docker tag test1 thrisha1012/devops-image
     docker push thrisha1012/devops-image
     docker-compose up -d
